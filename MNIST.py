fd908603bd3bf552223df06e6415ea1096d4c5c3
Ojas Gadre <ojasgadre@gmail.com>
2025-04-13 12:02:15 -0400

    Option to accelerate using cuda. Implemented validation

--- MNIST.py
+++ MNIST.py
@@ -1,5 +1,6 @@
 
-
+import time
+import torch 
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.datasets as dsets
@@ -8,9 +9,11 @@ from torch.utils.data import DataLoader
 from matplotlib import pyplot as plt
 
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 transform = transforms.ToTensor()
 
-mnist_trainset = dsets.MNIST(root='./data', train=True, download=True, transform=transform)
+mnist_trainset = dsets.MNIST(root='./data', train=True, download=True, transform=transform)  
 mnist_validate = dsets.MNIST(root='./data', train=False, download=True, transform=transform)
 
 training_set   = DataLoader(mnist_trainset, batch_size=100)
@@ -23,24 +26,45 @@ model = nn.Sequential(
     nn.Linear(256, 256),
     nn.ReLU(),
     nn.Linear(256, 10)
-)
+).to(device)
 
 
-optimizer = optim.SGD(model.parameters(), lr=0.01)
+optimizer     = optim.Adam(model.parameters(), lr=0.01)
 losses = []
+
+start_time = time.time()
 
 for epoch in range(10):
     for x, y in training_set:
-        x = x.view(-1, 784)
+        x, y  = x.view(-1, 784).to(device), y.to(device)
         y_pred = model(x)
         loss = nn.functional.cross_entropy(y_pred, y)
         losses.append(loss.item())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+    print(f'Epoch: {epoch+1}, Loss: {loss.item():.4f}')
 
+end_time = time.time()
 
 plt.plot(losses)
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.title('Training Loss')
 plt.show()
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for x, y in validation_set:
+        x, y = x.view(-1, 784).to(device), y.to(device)
+        y_pred = model(x)
+        predicted = torch.argmax(y_pred, dim=1)
+        correct += (predicted == y).sum().item()
+        total += y.size(0)
+
+accuracy = correct / total * 100
+print(f'Trained model in: {end_time - start_time} seconds\nWith an Validation Accuracy of: {accuracy:.2f}%')
 
 
